333fe8200a06d14df14cb835aefc0069fec899d3
TSAR <qixuanzhao@outlook.com>
2026-08-07 04:13:16 +0800

    link vulkan

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <QApplication>
 #include "mainwindow.h"
+#include <vulkan/vulkan.h>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    std::cout << "Available Vulkan extensions: " << extensionCount << std::endl;
+
     QApplication a(argc, argv);
 
     MainWindow w;
